be4183814d4fcd8acca59702fcc6552eda712cd5
Yu Ji <sophia.jiyu@gmai.com>
2023-09-20 14:12:00 +0000

    task 2 status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+// request URL
+const url = process.argv[2];
+
+request.get(url, (err, response) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    const code = response.statusCode;
+    console.log(`Code: ${code}`);
+  }
+});
