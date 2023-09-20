f35d23d5d081912c73cdab0a4fc661b6ea48e353
Yu Ji <sophia.jiyu@gmai.com>
2023-09-20 13:45:26 +0000

    update task 0 and task 1

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const args = process.argv[2];
 fs.readFile(args, 'utf8', (err, data) => {
   if (err) {
-    console.error(error);
+    console.error(err);
     return;
   }
   console.log(data);
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filename = process.argv[2];
 const strint_to_write = process.argv[3];
 
-fs.writeFile(filename, strint_to_write, 'utf8', (err) => {
+fs.writeFile(filename, strint_to_write, (err) => {
   if (err) {
     console.log(err);
   }
