c9b4f58e916d2dd7a57292f7c8557626ffc25fc3
Yu Ji <sophia.jiyu@gmai.com>
2023-09-20 14:14:37 +0000

    semistandard for task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -7,8 +7,7 @@ const url = process.argv[2];
 request.get(url, (err, response) => {
   if (err) {
     console.log(err);
-  }
-  else {
+  } else {
     const code = response.statusCode;
     console.log(`code: ${code}`);
   }
